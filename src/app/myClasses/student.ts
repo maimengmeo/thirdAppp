dd813a9d59ba54f2b62a7686b147a81e966e6e17
Mai Pham <mai291294@gmail.com>
2023-03-24 11:32:18 -0400

    student class

---
+++ src/app/myClasses/student.ts
@@ -0,0 +1,9 @@
+export class Student {
+    sid!: number;
+    sname!: string;
+    slogin!: string;
+    scampus!: string;
+}
+
+//!: postfix which tells Angular that this will never be null
+//can also just create the class and include a constructor
