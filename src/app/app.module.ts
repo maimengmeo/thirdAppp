dd7f806701838ec56fc31615cd63a3155fc22dca
Mai Pham <mai291294@gmail.com>
2023-03-24 11:27:15 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadfootComponent } from './headfoot/headfoot.component';
+import { HeaderComponent } from './headfoot/header/header.component';
+import { FooterComponent } from './headfoot/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadfootComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/headfoot/headfoot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headfoot',
+  templateUrl: './headfoot.component.html',
+  styleUrls: ['./headfoot.component.css']
+})
+export class HeadfootComponent {
+
+}
